f0ecaf328a693c50edb34357b6397abddf0a58b5
Bhumika <120472179+BHUMIKA-2110@users.noreply.github.com>
2025-04-25 08:42:37 +0530

    Add files via upload

---
+++ Task 2-Assign. 3.py
@@ -0,0 +1,9 @@
+n=int(input("Enter the number: "))
+from math import *
+Square=(sqrt(n))
+print("Square root: ",Square)
+loge=(log(n))
+print("Logarithm: ",loge)
+sine=(sin(n))
+print("Sine: ",sine)
+
